a9a753cd282394a77aa2b5c2c07cb5c940e2dc9d
yousonnet <orionxiaohan@gmail.com>
2023-12-16 19:19:14 +0800

    node1

--- node.ts
+++ node.ts
@@ -1,13 +1,18 @@
 import { ethers, Wallet } from "ethers";
 import "dotenv/config";
 
-let node_provider = new ethers.JsonRpcProvider("https://rpc.maplabs.io");
-let data = "0x";
+let node_provider = new ethers.JsonRpcProvider(
+  "https://cronos-evm.publicnode.com"
+);
+let data =
+  "0x646174613a2c7b2270223a226372632d3230222c226f70223a226d696e74222c227469636b223a2263726f73222c22616d74223a2231303030227d";
 async function main() {
   let node_blockNumber = await node_provider.getBlockNumber();
+  console.log("node_blockNumber", node_blockNumber);
   let private_key = process.env.PRIVATE_KEY!;
   let wallet = new ethers.Wallet(private_key, node_provider);
   let nonce = await wallet.getNonce();
+  console.log(nonce);
   while (true) {
     try {
       let transaction = {
