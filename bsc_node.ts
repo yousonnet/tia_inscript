c27db1b6bbcd596c9da3c7dce4a9e3caa2f6fd85
yousonnet <orionxiaohan@gmail.com>
2023-12-18 00:12:57 +0800

    add counter

--- bsc_node.ts
+++ bsc_node.ts
@@ -1,6 +1,8 @@
 import { Wallet, ethers } from "ethers";
 import "dotenv/config";
-let node_provider = new ethers.JsonRpcProvider("https://rpc-bsc.48.club");
+let node_provider = new ethers.JsonRpcProvider(
+  "https://bsc-dataseed1.bnbchain.org"
+);
 let data =
   "0x646174613a2c7b2270223a226273632d3230222c226f70223a226d696e74222c227469636b223a22736f6669222c22616d74223a2234227d";
 
@@ -10,7 +12,7 @@ async function main() {
   for (let i of keys) {
     wallets.push(new ethers.Wallet(i, node_provider));
   }
-
+  let counter = 0;
   while (true) {
     let txs: any[] = [];
     for (let wallet of wallets) {
@@ -20,7 +22,14 @@ async function main() {
       });
       txs.push(tx);
     }
-    await Promise.all(txs);
+    await Promise.all(txs)
+      .then((res) => {
+        counter++;
+        console.log(counter);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 }
 main();
